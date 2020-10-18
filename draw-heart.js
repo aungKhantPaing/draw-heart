c9de919b3f7ad0b651dc3cc82b91e27dcd8d160a
aungKhantPaing <mgakpmail@gmail.com>
2020-10-18 14:14:04 +0700

    code refactor

---
+++ draw-heart.js
@@ -0,0 +1,42 @@
+const drawHeart = (n) => {
+    console.log("For input", n, ":");
+    for (let i = -n; i <= n * 2; i++) {
+        // -3-2-10123456
+        let print = "";
+        // 012321-1-2-3
+        const h = n - Math.abs(i);
+        // -3-2-10123210
+        const b = n - Math.abs(i - n);
+        const sZero = i % n === 0 || i > n ? "" : "0";
+        const wing = getPLine(h);
+        const lWing = 0 + wing + sZero;
+        const rWing = sZero + wing + (i === n * 2 ? "" : "0");
+
+        let bot;
+        if (i < 0) bot = getSpace(n * 2 - h * 2 - 1);
+        else if (i === 0) bot = "0";
+        else bot = getPLine(b);
+
+        print += getSpace(Math.abs(i)) + lWing + bot + rWing;
+
+        console.log(...print);
+    }
+};
+
+const getPLine = (lineNo) => {
+    let line = "";
+    for (let i = -(lineNo - 1); i < lineNo; i++) {
+        line += lineNo - Math.abs(i);
+    }
+    return line;
+};
+
+const getSpace = (total) => {
+    let space = "";
+    for (let i = 0; i < total; i++) {
+        space += " ";
+    }
+    return space;
+};
+
+module.exports = drawHeart;
